b9da22c6d33cad3bc297d32ed1d21606163c9b71
Alex Stone <aloecend@gmail.com>
2021-08-02 05:34:49 -0400

    WIP: Initial commit of vector. NO TESTS.

---
+++ source/vector/vector.py
@@ -0,0 +1,21 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def get_value(self, coordinate:str) -> int:
+        if coordinate == 'x':
+            return self.x
+        elif coordinate == 'y':
+            return self.y
+        else:
+            raise ValueError
+
+    def scalar_multiplication(self, scalar:float):
+        return Vector(self.x * scalar, self.y * scalar)
+    
+    def add_vector(self, vector):
+        return Vector(self.x + vector.x, self.y + vector.y)
+    
+    def dot_product(self, vector) -> int:
+        return self.x * vector.x + self.y * vector.y
